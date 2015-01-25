8e665db52eec8a48fbad7791577381a0ff2528c5
kalospiles@gmail.com <kalospiles@gmail.com>
2015-01-25 12:35:24 +0100

    added room creation feature

--- app.js
+++ app.js
@@ -15,25 +15,34 @@ app.get('/', function (req, res) {
 var usernames = {};
 
 // rooms which are currently available in chat
-var rooms = ['room1','room2','room3'];
+var rooms = [];
 
 io.sockets.on('connection', function (socket) {
 	
 	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(username){
+	socket.on('adduser', function(room){
+		// generates random username
+		var username = Math.random().toString(36).substring(7);
 		// store the username in the socket session for this client
 		socket.username = username;
 		// store the room name in the socket session for this client
-		socket.room = 'room1';
+		socket.room = room;
 		// add the client's username to the global list
 		usernames[username] = username;
-		// send client to room 1
-		socket.join('room1');
+		// send client to selected room
+		socket.join(room);
+
+		// add room to array if not exists.
+		var roomAlreadyExists = rooms.indexOf(room);
+		if(roomAlreadyExists === -1){
+			rooms.push(room);
+		}
+
 		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
-		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-		socket.emit('updaterooms', rooms, 'room1');
+		socket.emit('updatechat', 'SERVER', 'you have connected to ' + room);
+		// echo to selected room that a person has connected to it
+		socket.broadcast.to(room).emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.emit('updaterooms', rooms, room);
 	});
 	
 	// when the client emits 'sendchat', this listens and executes
